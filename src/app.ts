02100925223949de84a26f736bdaa52f4ede0c61
Ramin Banihashemi <a@ramin.it>
2021-03-19 01:09:34 +0100

    Added helmet and logger for koa

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import Koa from 'koa';
+import helmet from 'koa-helmet';
+import logger from 'koa-pino-logger';
 import router from '@app/router';
 
 const app = new Koa();
+
+/* Security headers */
+app.use(helmet());
+
+/* Logger */
+app.use(logger());
 
 /* Load routes */
 app.use(router.routes());
